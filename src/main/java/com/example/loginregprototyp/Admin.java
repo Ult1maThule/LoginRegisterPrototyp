c8ee11130424f3d1036b290aadb6ef817edc513c
Ult1maThule <mauriceheimbach@gmail.com>
2022-04-15 20:48:00 +0200

    .

--- src/main/java/com/example/loginregprototyp/Admin.java
+++ src/main/java/com/example/loginregprototyp/Admin.java
@@ -1,4 +1,7 @@
 package com.example.loginregprototyp;
 
 public class Admin extends Person {
+    public Admin(String username, String password) {
+        super(username, password);
+    }
 }
--- src/main/java/com/example/loginregprototyp/User.java
+++ src/main/java/com/example/loginregprototyp/User.java
@@ -1,4 +1,7 @@
 package com.example.loginregprototyp;
 
 public class User extends Person {
+    public User(String username, String password) {
+        super(username, password);
+    }
 }
