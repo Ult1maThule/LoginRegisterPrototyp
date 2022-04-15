cfad12f0f55f85936155714edb181d05031a3799
Ult1maThule <mauriceheimbach@gmail.com>
2022-04-15 20:51:01 +0200

    person, admin, user done

--- src/main/java/com/example/loginregprototyp/Admin.java
+++ src/main/java/com/example/loginregprototyp/Admin.java
@@ -1,7 +1,19 @@
 package com.example.loginregprototyp;
 
 public class Admin extends Person {
-    public Admin(String username, String password) {
+    private int adminID;
+
+    public Admin(String username, String password, int adminID) {
         super(username, password);
+
+        this.adminID = adminID;
+    }
+
+    public int getAdminID() {
+        return adminID;
+    }
+
+    public void setAdminID(int adminID) {
+        this.adminID = adminID;
     }
 }
--- src/main/java/com/example/loginregprototyp/User.java
+++ src/main/java/com/example/loginregprototyp/User.java
@@ -1,7 +1,31 @@
 package com.example.loginregprototyp;
 
+import java.util.Date;
+
 public class User extends Person {
-    public User(String username, String password) {
+    private Date dateOfBirth;
+    private String email;
+
+    public User(String username, String password, Date dateOfBirth, String email) {
         super(username, password);
+
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
