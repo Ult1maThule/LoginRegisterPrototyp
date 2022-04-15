fe6aa1df0e869a39ac44e0fd46cd6440c5c403b1
VinoS2k <vinusan.sivalingam@stud.uni-duisburg-essen.de>
2022-04-15 21:31:11 +0200

    Admin user person

--- src/main/java/com/example/loginregprototyp/Admin.java
+++ src/main/java/com/example/loginregprototyp/Admin.java
@@ -1,10 +1,12 @@
 package com.example.loginregprototyp;
 
+
 public class Admin extends Person {
 
 
     public Admin(String firstname,String lastname,String username, String email, String password)  {
         super(firstname,lastname,username,email,password);
+
 
     }
 
