f8f61887eff54fab2b43bccfc73c976501483eb8
VinoS2k <vinusan.sivalingam@stud.uni-duisburg-essen.de>
2022-04-16 12:53:28 +0200

    registerView fertig

--- src/main/java/com/example/loginregprototyp/RegisterController.java
+++ src/main/java/com/example/loginregprototyp/RegisterController.java
@@ -1,11 +1,17 @@
 package com.example.loginregprototyp;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.InputMethodEvent;
+
+import java.util.EventListener;
 
 public class RegisterController {
 
@@ -47,7 +53,27 @@ public class RegisterController {
     private TextField usernameTextfield;
 
     @FXML
-    private TextField vornameTextfield;
+    private TextField firstnameTextfield;
+
+
+
+    public void initialize(){
+       checkNameFormat(firstnameTextfield);
+       checkNameFormat(lastnameTextfield);
+    }
+
+    private void checkNameFormat(TextField name){
+        name.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if(!name.getText().matches("[a-zA-Z]+")){
+                    nameErrorLabel.setText("Haben Sie Ihren Namen richtig eingegeben?");
+                } else {
+                    nameErrorLabel.setText(" ");
+                }
+            }
+        });
+    }
 
 }
 
