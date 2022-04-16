503f9d7b4997f321bf3d8f5282f27ba86ded781a
VinoS2k <vinusan.sivalingam@stud.uni-duisburg-essen.de>
2022-04-16 11:37:15 +0200

    registerView fertig

--- src/main/java/com/example/loginregprototyp/RegisterController.java
+++ src/main/java/com/example/loginregprototyp/RegisterController.java
@@ -1,11 +1,53 @@
 package com.example.loginregprototyp;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class RegisterController {
+
+
+    @FXML
+    private Label adminBestätigungLabel;
+
+    @FXML
+    private PasswordField confirmPasswordfield;
+
+    @FXML
+    private Button continueButton;
+
+    @FXML
+    private Label emailTakenError;
+
+    @FXML
+    private TextField emailTextfield;
+
+    @FXML
+    private TextField lastnameTextfield;
+
+    @FXML
+    private Label nameErrorLabel;
+
+    @FXML
+    private Label passwordErrorLabel;
+
+    @FXML
+    private PasswordField setPasswordfield;
+
+    @FXML
+    private CheckBox showPasswordCheckBox;
+
+    @FXML
+    private Label usernameTakenLabel;
+
+    @FXML
+    private TextField usernameTextfield;
 
     @FXML
     private TextField vornameTextfield;
 
 }
+
